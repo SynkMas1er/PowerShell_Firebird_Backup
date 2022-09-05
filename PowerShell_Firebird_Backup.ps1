cc665a24101ea0580f7d4346c791a1fea07030c2
SynkMas1er <vladimir.kuzzhel@gmail.com>
2022-09-05 22:48:21 +0300

    Ver 0.21. fixed bugs

--- PowerShell_Firebird_Backup.ps1
+++ PowerShell_Firebird_Backup.ps1
@@ -3,9 +3,9 @@
 # Creator: Volodja                    
 # CreationDate: 30.08.2022                              
 # LastModified: 05.09.2022                               
-# Version: 0.2
+# Version: 0.21
 # GitHub: https://github.com/SynkMas1er/PowerShell_Firebird_Backup
-# PSVersion tested: 5
+# PSVersion tested: 3
 #
 #
 #
@@ -44,14 +44,32 @@ function Out-Logger ($text,$allow=0) {
   
 Out-Logger "Starting backup"
 
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "Error running script: RUN AS ADMIN OR SYSTEM ACCOUNT!"
+    Out-Logger "Error running script: RUN AS ADMIN OR SYSTEM ACCOUNT!"
+    Out-Logger 1 1
+    Exit
+}
+
 try {
-    Stop-Service -Name $Servicename -ErrorAction Stop
+    Out-Logger "Stopping services..."
+    Stop-Service -Name $Servicename -ErrorAction Stop  
 }
 catch {
    Out-Logger "Error: $_" 
    Out-Logger 1 1
    Exit                                                                                                     # Exit if no services there
 }
+
+Start-Sleep -Seconds 3
+
+if ((Get-Service $Servicename).Status -eq 'Running') {
+    Start-Sleep -Seconds 10
+    if ((Get-Service $Servicename).Status -eq 'Running') {
+        Start-Sleep -Seconds 10
+    }
+}
+    
 if (Test-Path $DBFile) {
     try {
         if (Test-Path "$env:ProgramFiles\7-Zip\7z.exe") { 
@@ -63,6 +81,9 @@ if (Test-Path $DBFile) {
                     Out-Logger "Error: 7zip reported some error. Deleting empty 7z file..."
                     Remove-Item "$Dest$Date.XNS.FDB.backup.7z"
                 }
+                else {
+                    Out-Logger "File: $Filename created."
+                }
         }
         elseif (Test-Path "$env:ProgramFiles\WinRAR\winrar.exe") {
             Set-Alias Compressor "$env:ProgramFiles\WinRAR\winrar.exe"
@@ -72,24 +93,25 @@ if (Test-Path $DBFile) {
             if (-not (Test-Path $Dest$Filename)) {
                     Out-Logger "Error: WinRar reported some error."
             }
+            else {
+                Out-Logger "File: $Filename created."
+            }
         }  
         else {
             Out-Logger "7Zip and WinRAR not found trying to copy as is..."
             $Filename = "$Date.XNS.FDB.backup"
             Copy-Item -Path $DBFile -Destination "$Dest$Date.XNS.FDB.backup" -ErrorAction Stop              # Copy if no archiver found
-            
+            Out-Logger "File: $Filename created."
         }
     }
     catch {
         Out-Logger "Error: $_"
-        Out-Logger 1 1
     }
 }
 else {
     Out-Logger ("Error: File $DBFile not found")
 }
 
-Out-Logger "File: $Filename created."
 
 try {
     Start-Service -Name $Servicename -ErrorAction Stop                                                      # Starting service
@@ -108,8 +130,10 @@ catch {
     }
 }
 
-if ((Get-Service $Servicename).Status -eq 'Running') {                                                      # Check service running
-    Out-Logger "Service is started ok" 
+foreach ($i in $Servicename) {
+    if ((Get-Service $i).Status -eq 'Running') {                                                            # Check service running
+        Out-Logger "Service $i is started ok" 
+    }
 }
 
 if (Test-Path "$Dest$Filename") {                                                                           # Dele old files if backup created
@@ -120,3 +144,4 @@ if (Test-Path "$Dest$Filename") {                                               
 else {
     Out-Logger "Old files are not deleted due to missing new backup file"
 }
+Out-Logger 1 1
